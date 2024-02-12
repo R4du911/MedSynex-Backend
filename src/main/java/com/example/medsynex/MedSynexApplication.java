dfee45017e68dc0d883c847903001e39916a57b5
Radu Siderias <radusiderias@yahoo.com>
2024-02-12 11:55:17 +0200

    Initial commit

---
+++ src/main/java/com/example/medsynex/MedSynexApplication.java
@@ -0,0 +1,13 @@
+package com.example.medsynex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSynexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedSynexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medsynex/MedSynexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medsynex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSynexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
