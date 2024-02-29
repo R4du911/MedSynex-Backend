e6ea9498e8bf18c259e3a266cceb0653930907a3
Radu Siderias <radusiderias@yahoo.com>
2024-02-29 20:05:01 +0200

    forgot annotation for get request in specialization controller

--- src/main/java/com/example/medsynex/controller/SpecializationController.java
+++ src/main/java/com/example/medsynex/controller/SpecializationController.java
@@ -5,6 +5,7 @@ import com.example.medsynex.service.SpecializationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class SpecializationController {
     @Autowired
     private SpecializationService specializationService;
 
+    @GetMapping
     public ResponseEntity<List<Specialization>> getAllSpecializations() {
         return new ResponseEntity<>(specializationService.getAllSpecializations(), HttpStatus.OK);
     }
