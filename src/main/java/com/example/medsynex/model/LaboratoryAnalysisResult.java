b192e89712052aba42eca68d3cf5fd2a252a3ff2
Radu Siderias <radusiderias@yahoo.com>
2024-04-25 16:53:32 +0300

    create laboratory analysis result entity

---
+++ src/main/java/com/example/medsynex/model/LaboratoryAnalysisResult.java
@@ -0,0 +1,48 @@
+package com.example.medsynex.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "laboratory_results")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LaboratoryAnalysisResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "patient_cnp")
+    private Patient patient;
+
+    private LocalDate createDate;
+    private LocalDate updateDate;
+
+    private Double glucose;
+    private Double insulin;
+    private Double cholesterol;
+    private Double triglyceride;
+    private Double haemoglobin;
+    private Double leukocyteCount;
+    private Double plateletCount;
+    private Double totalCalcium;
+    private Double totalMagnesium;
+    private Double serumIron;
+    private Double serumSodium;
+    private Double serumPotassium;
+
+    private String laboratoryName;
+
+    private boolean diabetesRisk;
+
+    private String remarks;
+}
